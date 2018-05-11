91e15fc70eb84023098fd3deb095b64e88995dde
g-arais <gustavo@unorobotica.com.br>
2018-05-11 09:49:50 -0300

    Add files via upload

--- www/splashscreen.js
+++ www/splashscreen.js
@@ -29,10 +29,10 @@ var splashscreen = {
         exec(null, null, "SplashScreen", "hide", []);
     },
     show_spinner:function() {
-        exec(null, null, "Spinner", "show", []);
+        exec(null, null, "SplashScreen", "show", []);
     },
     hide_spinner:function() {
-        exec(null, null, "Spinner", "hide", []);
+        exec(null, null, "SplashScreen", "hide", []);
     }
 };
 
